d993b1557a056d2186f51d73ab4d1c6932f82169
rodrigonza92 <rodrigo.e.gonza@gmail.com>
2024-10-30 21:26:49 -0300

    Primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// 0xB24B46F0Bbb64489De5677557C3833dc0A92aFf2
+pragma solidity 0.8.24; // En esta parte defino la version del compilador.
+
+// A partir de aqui coloco la logica de mi contrato.
+contract hola_mundo {
+    string public saludo = "Hola desde Salta"; // Declaro el contenido de mi variable saludo.
+}
+
